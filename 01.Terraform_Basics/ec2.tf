3338f103d5f69846fdb972a4dba5bcda35684665
Shreyan Maity <shreyan.maity@outlook.com>
2024-10-30 10:33:44 +0530

    Update ec2.tf

--- 01.Terraform_Basics/ec2.tf
+++ 01.Terraform_Basics/ec2.tf
@@ -1,11 +1,11 @@
 #this is code to create 10 "Ubuntu" Ec2 instances with name "TestEC2" type of "t3.micro" in "ap-south-1"(Mumbai) location
 
 resource "aws_instance" "name" {
-  ami = "ami-0dee22c13ea7a9a67" #ubuntu ami id for ap-south-1 location
-  instance_type = "t3.micro" #ex. t2micro, t3.mircro, t2.small
-  #count = 10 #contus is used to create a set number of instances
+  ami = "ami-0dee22c13ea7a9a67"   #ubuntu ami id for ap-south-1 location
+  instance_type = "t3.micro"   #ex. t2micro, t3.mircro, t2.small
+  count = 10   #contus is used to create a set number of instances
 
   tags = {
-    Name = "TestEC2"
+    Name = "TestEC2_instance"
   }
 }
